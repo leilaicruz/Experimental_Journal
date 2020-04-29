b133edf788bb6e75d3b26c12077610cf5d503d34
Leila Iñigo De La Cruz - TNW <linigodelacruz@tudelft.net>
2020-04-29 21:10:06 +0200

    how common properties affect SL

--- journal/2020-04/MakeFile.sh
+++ journal/2020-04/MakeFile.sh
@@ -1,1 +1,1 @@
-pandoc  2020-04-08-summary-FACs-experiments.md   -s --mathjax  --filter=pandoc-crossref.exe --filter=pandoc-citeproc.exe   -o  Facs-summary-04-2020.pdf
+pandoc  2020-04-29-how-commons-properties-are-useful-to-predict-SL.md   -s --mathjax  --filter=pandoc-crossref.exe --filter=pandoc-citeproc.exe   -o  commmon2SL.pdf
